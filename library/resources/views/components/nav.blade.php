379e787cc835695049477157eadf4acd08ac768f
alicecroce <alicecroce2@gmail.com>
2023-06-11 16:56:24 +0200

    Sezione logout completa

--- library/resources/views/components/nav.blade.php
+++ library/resources/views/components/nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Book Shelter</a>
+        <a class="navbar-brand" href="{{ route('books.home') }}">Book Shelter</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -8,24 +8,42 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="{{ route('books.home') }}">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('books.create') }}">Inserisci un libro</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                        aria-expanded="false">
-                        Benvenuto ospite
-                    </a>
+                    @auth
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Benvenuto {{ Auth::user()->email }}
+                        </a>
+                    @else
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Benvenuto
+                        </a>
+                    @endauth
 
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="{{ route('register') }}">Registrati</a></li>
-                        <li><a class="dropdown-item" href="{{ route('login') }}">Login</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        @auth
+                            <li>
+                                <form action="{{ route('logout') }}" method="POST">
+                                    @csrf
+                                    <button class="dropdown-item"
+                                        onclick="event.preventDefault(); this.closest('form').submit();">Logout
+                                    </button>
+                                </form>
+                            </li>
+                        @else
+                            <li>
+                                <a class="dropdown-item" href="{{ route('register') }}">Registrati</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="{{ route('login') }}">Login</a>
+                            <li>
+                            @endauth
                     </ul>
                 </li>
             </ul>
